8a0bad47a0cc17bd965f5b98ab19bef04011ecd6
BekfastStealer <47260296+BekfastStealer@users.noreply.github.com>
2023-02-03 21:33:13 -0500

    Lil change

---
+++ Rutabaga/Assets/Scripts/MovementController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float LRInput = Input.GetAxis("Horizontal");
+        print(LRInput);
+    }
+}
