d1e869aa769875a4597aafc92bcb9346564d0253
BekfastStealer <47260296+BekfastStealer@users.noreply.github.com>
2023-02-03 22:03:12 -0500

    GrabHook can attach to an arbitrary point

--- Rutabaga/Assets/Scripts/MovementController.cs
+++ Rutabaga/Assets/Scripts/MovementController.cs
@@ -4,17 +4,31 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
-    
+    Rigidbody2D pBody;
+    Transform hookPoint;
+    DistanceJoint2D hookEnforcer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        pBody = GetComponent<Rigidbody2D>();
+        hookPoint = GameObject.Find("HookPoint").transform;
+        hookEnforcer = GetComponent<DistanceJoint2D>();
+        hookEnforcer.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float LRInput = Input.GetAxis("Horizontal");
-        print(LRInput);
+        float joystick = Input.GetAxis("Horizontal");
+        pBody.AddTorque(-joystick * Time.deltaTime * 5, ForceMode2D.Impulse);
+        if (Input.GetMouseButtonDown(0))
+        {
+            hookPoint.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            hookEnforcer.enabled = true;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            hookEnforcer.enabled = false;
+        }
     }
 }
