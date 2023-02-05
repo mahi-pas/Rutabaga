4f68102ab3f2afac66eb3eb681e3ea5b8572ac89
Mahi <mahipasarkar@gmail.com>
2023-02-04 19:27:38 -0500

    Player Health implemented!

---
+++ Rutabaga/Assets/Scripts/HurtPlayer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtPlayer : MonoBehaviour
+{
+    public int damage;
+    public bool respawnsPlayer;
+    public bool destroySelf;
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        Debug.Log("hit");
+        if(other.gameObject.tag == "Player"){
+            Debug.Log("hitPlayer");
+            PlayerHealth ph = other.gameObject.GetComponent<PlayerHealth>();
+            if(ph == null) return;
+            Debug.Log("found PlayerHealth");
+            ph.TakeDamage(damage);
+            if(respawnsPlayer) ph.Respawn();
+            if(destroySelf) Destroy(gameObject);
+        }
+    }
+}
---
+++ Rutabaga/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 3;
+    public int curHealth;
+
+    public HUD hud;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        curHealth = maxHealth;
+        if(hud == null) hud = GameObject.FindGameObjectsWithTag("HUD")[0].GetComponent<HUD>();
+    }
+
+    public void TakeDamage(int dmg){
+        SetHealth(curHealth - dmg);
+    } 
+
+    public void Respawn(){
+        Debug.Log("Respawn");
+    }
+
+    void Die(){
+        Debug.Log("You ran out of health!");
+    }
+
+    public void ResetHealth(){
+        SetHealth(maxHealth);
+    }
+
+    public void SetHealth(int h){
+        curHealth = Mathf.Max(h,0);
+        hud.SetHealth(curHealth);
+        if(curHealth == 0){
+            Die();
+        }
+    }
+
+
+}
